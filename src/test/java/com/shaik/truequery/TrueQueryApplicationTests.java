4056cf6b3244450a157be447901fc6784457d49d
Codewithshaik <imoxygod@gmail.com>
2025-07-13 21:49:54 +0530

    API model test added with port

--- src/test/java/com/shaik/truequery/TrueQueryApplicationTests.java
+++ src/test/java/com/shaik/truequery/TrueQueryApplicationTests.java
@@ -2,12 +2,20 @@ package com.shaik.truequery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TrueQueryApplicationTests {
 
+
     @Test
     void contextLoads() {
+    }
+    @Test
+    void sanityCheck() {
+        String projectName = "TrueQuery";
+        assertThat(projectName).isEqualTo("TrueQuery");
     }
 
 }
